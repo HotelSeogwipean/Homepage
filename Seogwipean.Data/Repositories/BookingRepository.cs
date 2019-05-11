36f38b9088561295cb2e8ca767ecf78d8bd9e012
hotelseogwipean <hotelseogwipean@naver.com>
2019-05-11 18:03:38 +0900

    UPDATE

--- Seogwipean.Data/Repositories/BookingRepository.cs
+++ Seogwipean.Data/Repositories/BookingRepository.cs
@@ -252,6 +252,7 @@ namespace Seogwipean.Data.Repositories
                     };
                     db.Booking.Add(newBooking);
                     db.SaveChanges();
+                    _logger.LogInformation(DateTime.Now + " || 시숙 예약 추가, 예약자명: " + userName + ", 체크인: " + startDate.ToShortDateString() + ", 체크아웃: " + endDate.Value.ToShortDateString());
                     return new LongResult
                     {
                         Result = Common.Success
