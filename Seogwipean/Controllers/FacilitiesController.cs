76dcdf110363bdb0cc618870d7dc2538441c0648
hotelseogwipean <hotelseogwipean@naver.com>
2019-08-19 16:48:10 +0900

    Copyright

---
+++ Seogwipean/Controllers/FacilitiesController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Seogwipean.Web.Controllers
+{
+    public class FacilitiesController : BaseController
+    {
+        private readonly ILogger _logger;
+
+        public FacilitiesController(ILoggerFactory loggerFactory)
+        {
+            _logger = loggerFactory.CreateLogger<FacilitiesController>();
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
