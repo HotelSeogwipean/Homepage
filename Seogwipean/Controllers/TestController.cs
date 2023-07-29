24d36b5c11770fa0ed2faf0f6569126d8254ad34
hotelseogwipean@naver.com <hotelseogwipean@naver.com>
2023-07-29 19:45:13 +0900

    t

--- Seogwipean/Controllers/TestController.cs
+++ Seogwipean/Controllers/TestController.cs
@@ -24,39 +24,26 @@ namespace Seogwipean.Web.Controllers
             return View();
         }
 
-        public class RequestModel
-        {
-            public string Source { get; set; }
-            public string Target { get; set; }
-            public string Text { get; set; }
-            public string Client { get; set; }
-            public string Secret { get; set; }
-        }
-
         [HttpGet("/Test/Papago")]
         [HttpPost("/Test/Papago")]
-        public IActionResult GetPapago(RequestModel vm)
+        public IActionResult GetPapago(string client, string secret, string source, string target, string inputs)
         {
-            if (vm.Source == "" || vm.Source == null)
+            string _client = client;
+            string _secret = secret;
+            string _source = source;
+            string _target = target;
+            string _query = inputs;
+            Console.WriteLine("Client " + _client + " Secret " + _secret + " Source " + _source + " Target " + _target + " Text " + _query);
+            if (_source == "" || _source == null)
             {
-                vm.Source = "ko";
-                vm.Target = "en";
-                vm.Text = "지금 진행중인 내용은 테스트를 위한 의미없는 번역입니다.";
+                _source = "ko";
+                _target = "en";
+                _query = "지금 진행중인 내용은 테스트를 위한 의미없는 번역입니다.";
                 // vm.Client = "xTvSfa4FtosiAGIJy7XE";
                 // vm.Secret = "qMuICLBMLp";
-                vm.Client = "Y9EjKoi9iVLwIRmq0C3d";
-                vm.Secret = "gEYBMfFMP5";
+                _client = "Y9EjKoi9iVLwIRmq0C3d";
+                _secret = "gEYBMfFMP5";
             }
-
-                //return Json(new LongResult {
-                //    Result = Common.Fail,
-                //    Reason = "REQUESET 내용이 없습니다."
-                //});
-            string _client = vm.Client;
-            string _secret = vm.Secret;
-            string _source = vm.Source;
-            string _target = vm.Target;
-            string _query = vm.Text;
 
             string url = "https://openapi.naver.com/v1/papago/n2mt";
             HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
@@ -67,10 +54,6 @@ namespace Seogwipean.Web.Controllers
             byte[] byteDataParams = Encoding.UTF8.GetBytes("source=" + _source + "&target=" + _target + "&text=" + _query);
             request.ContentType = "application/x-www-form-urlencoded";
             request.ContentLength = byteDataParams.Length;
-
-            Console.WriteLine("Client " + _client + " Secret " + _secret + " Source " + _source + " Target " + _target + " Text " + _query);
-            Console.WriteLine();
-
 
             Stream st = request.GetRequestStream();
             st.Write(byteDataParams, 0, byteDataParams.Length);
